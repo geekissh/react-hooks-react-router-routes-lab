47ce323f9f6e5c4269dc3825964ef7f58299e3af
Mark Kelsall <markkelsall1995@gmail.com>
2024-02-07 07:11:59 +0300

    finale

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavLink to="/" style={{ display: "block" }}>Home</NavLink>
+      <NavLink to="/movies" style={{ display: "block" }}>Movies</NavLink>
+      <NavLink to="/directors" style={{ display: "block" }}>Directors</NavLink>
+      <NavLink to="/actors" style={{ display: "block" }}>Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
